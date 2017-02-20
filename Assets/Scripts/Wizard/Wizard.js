b81f467d522e2fee5b223189b456cc707d104e54
Kevin Benson <benson_k3@denison.edu>
2017-02-20 10:51:25 -0500

    initial commit

---
+++ Assets/Scripts/Wizard/Wizard.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var jumping:boolean = false;
+var can_move:boolean = true;
+
+var speed:float = 0.25;
+
+function Start () {
+
+}
+
+function Update () {
+	Move();
+	Jump();
+
+	transform.rotation = Quaternion.identity;
+}
+
+function Move(){
+	if (can_move){
+		var x:int = Input.GetAxisRaw("Horizontal");
+		print(x*speed);
+		transform.Translate(x*speed,0,0);
+	}
+}
+
+function Jump(){
+	if (GetComponent.<Rigidbody2D>().velocity.y < 0.1 && GetComponent.<Rigidbody2D>().velocity.y > -0.1 )
+		jumping = false;
+
+	print(GetComponent.<Rigidbody2D>().velocity.y);
+
+	if (!jumping){
+		if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1"))
+			GetComponent.<Rigidbody2D>().AddForce(Vector3.up*300);
+			jumping = true;
+	}
+
+}
