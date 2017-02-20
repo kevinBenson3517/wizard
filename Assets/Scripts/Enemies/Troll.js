c6d3b248f32a0d8ee2b36d728c678fa74ddc487b
Carol Vitellas <vitell_C1@denison.edu>
2017-02-20 18:39:38 -0500

    Carol's changes

--- Assets/Scripts/Enemies/Troll.js
+++ Assets/Scripts/Enemies/Troll.js
@@ -10,3 +10,12 @@ function Start () {
 function Update () {
 	transform.position.x += -speed;
 }
+
+function OnCollisionEnter2D(col:Collision2D)
+{	Application.LoadLevel("GameOver1");	//THIS
+	if(col.gameObject.tag=="fireball")
+	{
+		GetComponent.<Renderer>().enabled = false;
+		Destroy(gameObject);
+	}
+}
