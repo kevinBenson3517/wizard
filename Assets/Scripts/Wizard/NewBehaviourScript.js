b9a6418d97fbe6326447a96a9152fae141104cf1
Kevin Benson <benson_k3@denison.edu>
2017-02-22 20:01:20 -0500

    Added preliminary map/camera tracking

---
+++ Assets/Scripts/Wizard/NewBehaviourScript.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	transform.position = new Vector3(transform.position.x, Mathf.Clamp (transform.position.y, 1.8,1.8), transform.position.z);
+}
