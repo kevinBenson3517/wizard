fb35a59bf58441ea4fc0eb1e908170223b868eaf
cc-sarafin <sarafi_c1@denison.edu>
2017-03-01 20:27:28 -0500

    added several enemies (enemy that moves side to side but not effected by gravity, a ball, and a ghost that travels in a circle)

---
+++ Assets/Scripts/Enemies/EvilBall.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var drop:GameObject;
+
+var force:int;
+
+var health:int;
+
+function Start () {
+	GetComponent.<Rigidbody2D>().AddForce(Vector3.left*force);
+}
+
+function Update () {
+
+}
+
+
+function OnCollisionEnter2D (hit : Collision2D){
+	if (hit.gameObject.CompareTag("fireball")){
+		health--;
+	}
+
+	if (health <= 0){
+		Instantiate(drop, transform.position, Quaternion.identity);
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Enemies/Ghost.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+import System.Math;
+
+var radius:float;
+var period:float;
+var scale:float;
+
+var center:Vector3;
+
+function Start () {
+	center = transform.position;
+	transform.position.x += radius;
+
+}
+
+function Update () {
+	transform.RotateAround(center, Vector3.back, period*Time.deltaTime);
+	transform.rotation.z = 0;
+}
