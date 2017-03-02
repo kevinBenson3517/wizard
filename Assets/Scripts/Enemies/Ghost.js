eabc118f3d70ba77282f0879692817694e30a035
Carol Vitellas <vitell_C1@denison.edu>
2017-03-02 14:02:44 -0500

    carols changes

--- Assets/Scripts/Enemies/Ghost.js
+++ Assets/Scripts/Enemies/Ghost.js
@@ -5,6 +5,10 @@ import System.Math;
 var radius:float;
 var period:float;
 var scale:float;
+var health:int;
+var drop:GameObject;
+
+var speed:float;
 
 var center:Vector3;
 
@@ -17,4 +21,19 @@ function Start () {
 function Update () {
 	transform.RotateAround(center, Vector3.back, period*Time.deltaTime);
 	transform.rotation.z = 0;
+}
+
+function OnCollisionEnter2D (hit : Collision2D){
+	if (hit.gameObject.CompareTag("fireball")){
+		health--;
+	}
+	if (hit.gameObject.CompareTag("Untagged")){
+		speed = -speed;
+		transform.localScale = Vector3.Scale(transform.localScale, Vector3(-1,1,1));
+	}
+
+	if (health <= 0){
+		Instantiate(drop, transform.position, Quaternion.identity);
+		Destroy(this.gameObject);
+	}
 }
