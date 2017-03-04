35dce9cf634281ee7eb2ee8263519dc009284208
Haley Nugent <Haley_Nugent@Haley-Nugents-MacBook-Pro.local>
2017-03-04 11:40:45 -0500

    10 coins = extra heart

--- Assets/Scripts/Wizard/Heart UI.js
+++ Assets/Scripts/Wizard/Heart UI.js
@@ -5,6 +5,7 @@ var lives : int;
 var health1 : Texture2D; //one life left
 var health2 : Texture2D; //two lives left
 var health3 : Texture2D; //full health
+var health4 : Texture2D; //extra health from coins
 
 function Start () {
 	Wizard.lives = 3;
@@ -15,11 +16,35 @@ function Update () {
 		GetComponent.<GUITexture>().texture = health1;
 	}
 
-	if(Wizard.lives == 2){
+	else if(Wizard.lives == 2){
 		GetComponent.<GUITexture>().texture = health2;
 	}
 	
-	if(Wizard.lives == 3){
+	else if(Wizard.lives == 3){
 		GetComponent.<GUITexture>().texture = health3;
 	}
+
+	if (Wizard.money >= 10){
+		AddHearts();
+		Wizard.money = Wizard.money - 10;
+	}
+}
+
+function AddHearts() {
+	/* if the wizard has 10 or more coins, gain a life and subtract 10 coins from count */
+	if(Wizard.lives == 1){
+		Wizard.lives++;
+		GetComponent.<GUITexture>().texture = health2;
+	}
+
+	else if(Wizard.lives == 2){
+		Wizard.lives++;
+		GetComponent.<GUITexture>().texture = health3;
+	}
+	
+	else if(Wizard.lives == 3){
+		Wizard.lives++;
+		GetComponent.<GUITexture>().texture = health4;
+	}
+
 }
