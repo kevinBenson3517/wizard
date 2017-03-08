0917b86ce43765f7d6fef0af964a38567c7b0d03
H. Nugent <nugent_h2@denison.edu>
2017-03-08 06:41:04 -0500

    moving platform

---
+++ Assets/Scripts/Misc/Moving Platform.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var speed:float;
+
+
+function Start () {
+
+
+}
+
+function Update () {
+	transform.position.x += -speed;
+}
+
+
+function OnCollisionEnter2D (hit : Collision2D){
+	if (hit.gameObject.CompareTag("mp")){
+		ChangeDirection();
+
+	}
+	print("hit");
+}
+
+function ChangeDirection(){
+	speed = -speed;
+	transform.localScale = Vector3.Scale(transform.localScale, Vector3(-1,1,1));
+}
