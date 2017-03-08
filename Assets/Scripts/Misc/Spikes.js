c28974cbe42ab36c57dc88b998b210a6199937dd
H. Nugent <nugent_h2@denison.edu>
2017-03-08 05:33:58 -0500

    trying to get my stuff back

--- Assets/Scripts/Misc/Spikes.js
+++ Assets/Scripts/Misc/Spikes.js
@@ -7,4 +7,10 @@ function OnTriggerEnter2D (other: Collider2D)
           other.gameObject.GetComponent(Wizard).lives -=1;
 		  print(other.gameObject.GetComponent(Wizard).lives);
       }
+      else changeDirection();
  }
+
+ function ChangeDirection(){
+	speed = -speed;
+	transform.localScale = Vector3.Scale(transform.localScale, Vector3(-1,1,1));
+}
